63525079d75f9ef060e24c31cef3c681b526df87
yourdeathgg <75530853+Devel54Pro@users.noreply.github.com>
2022-04-09 14:38:30 +0700

    Add files via upload
    GameSprite class aded

--- ping-pong.py
+++ ping-pong.py
@@ -7,6 +7,16 @@ pygame.init()
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, image, image_x, image_y, x, y):
+        self.image = pygame.transform.scale(pygame.image.load(image), (image_x, image_y))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def blit(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
 while True:
     clock.tick(fps)
     for event in pygame.event.get():
