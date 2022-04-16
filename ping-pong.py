589a599e25c988ed62b9c8ecadbb12d01fb2babf
yourdeathgg <75530853+Devel54Pro@users.noreply.github.com>
2022-04-16 14:19:32 +0700

    Add files via upload
    Victory conditions added, player tags added

--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,6 @@
 import pygame
 from random import choice
+from time import sleep
 
 width, height = 800, 600
 fps = 60
@@ -42,46 +43,73 @@ class Racket(GameSprite):
     def move(self, keys, controller):
         if controller == 'w,s':
             if keys[pygame.K_w] and self.rect.y > 0:
-                self.rect.y -= 5
+                self.rect.y -= 8
             if keys[pygame.K_s] and self.rect.y < 400:
-                self.rect.y += 5
+                self.rect.y += 8
         if controller == 'up,down':
             if keys[pygame.K_UP] and self.rect.y > 0:
-                self.rect.y -= 5
+                self.rect.y -= 8
             if keys[pygame.K_DOWN] and self.rect.y < 400:
-                self.rect.y += 5
+                self.rect.y += 8
 
 racket1 = Racket('platform.png', 46, 200, 0, 200)
 racket2 = Racket('platform.png', 46, 200, 754, 200)
 ball = Ball('ball.png', 50, 50, 375, 275)
 
+game = True
+res = None
 ticks = 300
 while True:
     clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
+    if game:
+        keys = pygame.key.get_pressed()
+        screen.fill((150, 150, 255))
 
-    keys = pygame.key.get_pressed()
-    screen.fill((150, 150, 255))
+        racket1.move(keys, 'w,s')
+        racket2.move(keys, 'up,down')
+        if ticks == 0:
+            ticks = 300
+            if ball.speedx > 0:
+                ball.speedx += 1
+            if ball.speedx < 0:
+                ball.speedx -= 1
+            if ball.speedy > 0:
+                ball.speedy += 1
+            if ball.speedy < 0:
+                ball.speedy -= 1
+        ball.move([racket1, racket2])
+        if ball.rect.x <= 0 or ball.rect.x >= 750:
+            game = False
+            if ball.rect.x <= 0:
+                res = 2
+            if ball.rect.x >= 750:
+                res = 1
 
-    racket1.move(keys, 'w,s')
-    racket2.move(keys, 'up,down')
-    if ticks == 0:
+        racket1.blit(screen)
+        racket2.blit(screen)
+        ball.blit(screen)
+
+        screen.blit(pygame.font.SysFont('Arial', 24).render('Player 1', 8, (255, 255, 255)), (2, 2))
+        screen.blit(pygame.font.SysFont('Arial', 24).render('Player 2', 8, (255, 255, 255)), (710, 2))
+
+        pygame.display.update()
+        ticks -= 1
+    else:
+        if res == 1:
+            screen.blit(pygame.font.SysFont('Arial', 36).render('Player 1 won!', 8, (255, 255, 255)), (320, 280))
+        if res == 2:
+            screen.blit(pygame.font.SysFont('Arial', 36).render('Player 2 won!', 8, (255, 255, 255)), (320, 280))
+        pygame.display.update()
+        sleep(3)
+        res = None
         ticks = 300
-        if ball.speedx > 0:
-            ball.speedx += 1
-        if ball.speedx < 0:
-            ball.speedx -= 1
-        if ball.speedy > 0:
-            ball.speedy += 1
-        if ball.speedy < 0:
-            ball.speedy -= 1
-    ball.move([racket1, racket2])
-
-    racket1.blit(screen)
-    racket2.blit(screen)
-    ball.blit(screen)
-
-    pygame.display.update()
-    ticks -= 1
+        ball.rect.x = 375
+        ball.rect.y = 275
+        ball.speedx = choice([5, -5])
+        ball.speedy = choice([5, -5])
+        racket1.rect.y = 200
+        racket2.rect.y = 200
+        game = True
